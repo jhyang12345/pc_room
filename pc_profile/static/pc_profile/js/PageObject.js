8af9bc6918226cd414b9b1f4ca9ae41c3ae87291
jhyang12345 <jhyang12345@naver.com>
2018-04-07 02:05:45 +0900

    implemented search and navigation

--- pc_profile/static/pc_profile/js/PageObject.js
+++ pc_profile/static/pc_profile/js/PageObject.js
@@ -135,7 +135,37 @@ function handleSearchQuery() {
   window.location.href = searchPageLink;
 }
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+	return Math.round(d*10)/10;
+}
+
+function getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+	return Math.round(d * 1000);
+}
 
 // Things that need to be on localStorage
 // 1. Current Location
--- pc_profile/static/pc_profile/js/results.js
+++ pc_profile/static/pc_profile/js/results.js
@@ -1,5 +1,56 @@
 document.addEventListener("DOMContentLoaded", function(evt) {
   pageObject  = new PageObject();
+  const templateText = document.querySelector("#result_template").innerHTML;
+  const template = Handlebars.compile(templateText);
+
+  const location = {lat: 37.56058516193408, lng: 127.03855991363525};
+  if(pageObject.storageAvailable) {
+    if(localStorage["lat"]) {
+      location["lat"] = parseFloat(localStorage["lat"]);
+      location["lng"] = parseFloat(localStorage["lng"]);
+    }
+  }
+
+  profileResults.sort(function(a, b) {
+    var keyA = getDistanceFromLatLonInKm(a["lat"], a["lng"], location["lat"], location["lng"]);
+    var keyB = getDistanceFromLatLonInKm(b["lat"], b["lng"], location["lat"], location["lng"]);
+    if(keyA > keyB) return 1;
+    else return 0;
+  })
+
+  for (profile of profileResults) {
+    let distance = getDistanceFromLatLonInKm(profile["lat"], profile["lng"], location["lat"], location["lng"]);
+    let distanceText = "";
+    if(distance < 1) {
+      distance = getDistanceFromLatLonInM(profile["lat"], profile["lng"], location["lat"], location["lng"]);
+      distanceText = distance + " m";
+    } else {
+      distanceText = distance + " km";
+    }
+    console.log(imageDict[profile["id"]]);
+    const templateObject = {
+      id: profile["id"],
+      title: profile["pc_title"] + " - " + profile["pc_subtitle"],
+      address: profile["address"],
+      distance: distanceText,
+    };
+    if(imageDict[profile["id"]]) {
+      templateObject["thumbnail"] = imageDict[profile["id"]][0];
+    }
+    document.querySelector(".container-content").innerHTML += template(templateObject);
+
+  }
+
+  $(".info-box").on("click tap", function(evt) {
+    console.log($(evt.currentTarget).attr("id"));
+    window.location.href = "/detail/" + $(evt.currentTarget).attr("id");
+  })
+
+  $(".info-box .info-thumbnail img").on("load", function(evt) {
+    const curImage = evt.target;
+    $(curImage).fadeTo(300, 1.0);
+    console.log("Loaded?");
+  });
 
 });
 
